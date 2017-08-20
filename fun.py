67f34297557f387469a4eb0e89204a7d8a7adc60
Lucas Foseca <lucasfonmiranda@gmail.com>
2017-08-20 15:52:26 -0300

    Try this

--- fun.py
+++ fun.py
@@ -5,3 +5,12 @@ def function():
 		print('Negative')
 n = int(input('Digite um valor qualquer: \n'))
 function() 
+
+# Try to do this
+# def function():
+# 	n = int(input("Enter one number: "))
+# 	if n > 0:
+# 		print ('Positive Number')
+# 	else:
+# 		print ('Negative Number')
+# function()
